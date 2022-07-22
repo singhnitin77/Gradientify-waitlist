e2d5ab9a991e46887b9fdee0f823fac79f42ecb1
Nitin Singh <nitinsinghofficial9@gmail.com>
2022-07-22 15:54:33 +0530

    https

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import axios from "axios";
-axios.defaults.baseURL = "http://gradientify-waitlist-backend.vercel.app/";
+axios.defaults.baseURL = "https://gradientify-waitlist-backend.vercel.app/";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
